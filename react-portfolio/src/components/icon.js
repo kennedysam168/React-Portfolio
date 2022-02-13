a585c7113127165280c4d10afa201da31feb43fc
Sam Kennedy <kennedysam168@gmail.com>
2022-02-12 22:47:31 -0500

    changed file structure

---
+++ react-portfolio/src/components/icon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+import icon from '/Users/samkennedy/Desktop/React-Portfolio/react-portfolio/src/icon-image.jpeg';
+
+const Icon = () => {
+    return (
+        <img src={icon} alt="icon" className="icon-image"/>
+    )
+}
+
+
+
+
+
+
+export default Icon;
